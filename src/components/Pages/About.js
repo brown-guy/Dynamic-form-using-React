e0e82216dfb49bb0b6cd5a363e270f6d6d38a625
Muhammad Malik <muhammadmalik645@gmail.com>
2023-04-29 14:38:23 +0500

    Test code

---
+++ src/components/Pages/About.js
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h1>Welcome to the About Us Page</h1>
+            <p>This is the About page of our website.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/components.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the home page of our website.</p>
+        </div>
+    );
+}
+function About() {
+    return (
+        < div >
+            <h1>Welcome to the About Page</h1>
+        </div >
+    );
+}
+
+function Contact() {
+    return (
+        < div >
+            <h1>Welcome to the contact Page</h1>
+        </div >
+    );
+
+}
+
+export { Home, About, Contact };
